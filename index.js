9cd6ba73373fde9569e3dcfaa0a95e6e0a162c39
Godfrey Karugu <karugugk@gmail.com>
2023-10-08 19:08:13 +0300

    first attempt at scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = bob;
+
+const leastFavoriteCustomer = 'James';
+
+function upperCaseCustomerName(){
+
+  return customerName.toUppercase();
+  customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+
+  bestCustomer = 'bob'
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer ="hassan";
+}
